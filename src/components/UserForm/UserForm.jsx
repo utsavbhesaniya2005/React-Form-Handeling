eec36f50ed541bc4da8bc929bfe610ac4f5a45d6
utsavbhesaniya2005 <ukbhesaniya01@gmail.com>
2024-10-25 09:06:25 +0530

    CRUD Added

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { getUsers } from "../../services/Helper";
+import generateUniqueId from "generate-unique-id";
 
 
 const UserForm = () => {
@@ -9,8 +10,8 @@ const UserForm = () => {
         fname : '',
         lname : '',
         email : '',
-        pass : '',
-        contact : '',
+        des : '',
+        dept : '',
         city : ''
     });
 
@@ -34,12 +35,12 @@ const UserForm = () => {
             obj.email = "Email Must Be Required...";
         }
 
-        if(userInput.pass == ''){
-            obj.pass = "Password Must Be Required...";
+        if(userInput.des == ''){
+            obj.des = "Designation Must Be Required...";
         }
 
-        if(userInput.contact == ''){
-            obj.contact = "Contact Must Be Required...";
+        if(userInput.dept == ''){
+            obj.dept = "Dept Must Be Required...";
         }
         
         if(userInput.city == ''){
@@ -60,23 +61,57 @@ const UserForm = () => {
         setUserInput({...userInput,[e.target.name] : e.target.value});
     }
 
+    const handleSelect = (selectId) => {
+        console.log("Select Id",selectId);
+    }
+
+    const handleEdit = (updateId) => {
+        
+        let editRec = storage.find((rec) => rec.id == updateId)
+        
+        setUserInput(editRec)
+    }
+
+    const handleDelete = (deleteId) => {
+        let deleteRec = storage.filter((rec) => rec.id != deleteId)
+
+        setStorage(deleteRec);
+    }
+
     const formSubmit = (e) => {
 
         e.preventDefault();
 
         if(validation()){
 
-            userInput.id = Math.floor(Math.random() * 1000);
+            if(userInput.id != ''){
+                
+                let updateRec = storage.map((rec) => {
+
+                    if(userInput.id == rec.id){
+                        return userInput;
+                    }else{
+                        return rec;
+                    }
+                })
+                setStorage(updateRec);
+            }else{
+
+                userInput.id = generateUniqueId({
+                    length: 4,
+                    useLetters: false
+                });
+                return setStorage([...storage, userInput]);
+            }
             
-            setStorage([...storage, userInput]);
 
             setUserInput({
                 id: '',
                 fname : '',
                 lname : '',
                 email : '',
-                pass : '',
-                contact : '',
+                des : '',
+                dept : '',
                 city : ''
             });
             setInputError({});
@@ -97,41 +132,47 @@ const UserForm = () => {
             <div className="container">
                 <form onSubmit={formSubmit} className="row g-3">
                     <div className="col-12">
-                        <input type="text" className="form-control" value={storage.id} hidden/>
+                        <input type="text" className="form-control" value={userInput.id} hidden />
                     </div>
                     <div className="col-md-6">
-                        <label className="form-label d-flex">First Name :</label>
-                        <input type="text" className="form-control" placeholder="Enter First Name"  name="fname" onChange={handleInput} value={userInput.fname} />
+                        <label className="form-label d-flex">Emp_Name :</label>
+                        <input type="text" className="form-control" placeholder="Enter Emp Name" name="fname" onChange={handleInput} value={userInput.fname} />
                         {inputError.fname ? <span>{inputError.fname}</span> : ''}
+                        <br/><br />
                     </div>
-                    <div className="col-md-6">
-                        <label className="form-label d-flex">Last Name :</label>
-                        <input type="text" className="form-control" placeholder="Enter Last Name"  name="lname" onChange={handleInput} /><br value={userInput.lname} />
+                    <div className="col-12">
+                        <label className="form-label d-flex">Address :</label>
+                        <textarea rows="5" cols="5" type="text" className="form-control" placeholder="Enter Address" name="lname" onChange={handleInput} value={userInput.lname} />
                         {inputError.lname ? <span>{inputError.lname}</span> : ''}
+                        <br/><br />
                     </div>
 
-                    <div className="col-12">
+                    <div className="col-7">
                         <label className="form-label d-flex">Email :</label>
-                        <input type="email" className="form-control" placeholder="Enter Your Email" name="email" onChange={handleInput} value={userInput.email} /><br /><br />
+                        <input type="email" className="form-control" placeholder="Enter Your Email" name="email" onChange={handleInput} value={userInput.email} />
                         {inputError.email ? <span>{inputError.email}</span> : ''}
+                        <br /><br />
                     </div>
 
-                    <div className="col-12">
-                        <label className="form-label d-flex">Password :</label>
-                        <input type="password" className="form-control" placeholder="Enter Your Password" name="pass" onChange={handleInput} value={userInput.pass} /><br /><br />
-                        {inputError.pass ? <span>{inputError.pass}</span> : ''}
+                    <div className="col-5">
+                        <label className="form-label d-flex">Designation :</label>
+                        <input type="text" className="form-control" placeholder="Enter Your Designation" name="des" onChange={handleInput} value={userInput.des} />
+                        {inputError.des ? <span>{inputError.des}</span> : ''}
+                        <br /><br />
                     </div>
 
-                    <div className="col-12">
-                        <label className="form-label d-flex">Contact :</label>
-                        <input type="text" className="form-control"  placeholder="Enter Your Contact" name="contact" onChange={handleInput} value={userInput.contact} /><br /><br />
-                        {inputError.contact ? <span>{inputError.contact}</span> : ''}
+                    <div className="col-6">
+                        <label className="form-label d-flex">Department :</label>
+                        <input type="text" className="form-control"  placeholder="Enter Your Department" name="dept" onChange={handleInput} value={userInput.dept} />
+                        {inputError.dept ? <span>{inputError.dept}</span> : ''}
+                        <br />
                     </div>
 
-                    <div className="col-12">
+                    <div className="col-6">
                         <label className="form-label d-flex">City :</label>
-                        <input type="text" className="form-control" placeholder="Enter Your City" name="city" onChange={handleInput}  value={userInput.city} /><br /><br /><br /><br />
+                        <input type="text" className="form-control" placeholder="Enter City" name="city" onChange={handleInput}  value={userInput.city} />
                         {inputError.city ? <span>{inputError.city}</span> : ''}
+                        <br /><br /><br />
                     </div>
 
                     <input type="submit" className="btn btn-primary" value={'Submit'}/>
@@ -144,8 +185,10 @@ const UserForm = () => {
                         <tr>
                             <th scope="col">Name</th>
                             <th scope="col">Email</th>
-                            <th scope="col">Contact</th>
+                            <th scope="col">Designation</th>
+                            <th scope="col">Department</th>
                             <th scope="col">City</th>
+                            <th scope="col">Action</th>
                         </tr>
                     </thead>
                     {
@@ -156,8 +199,13 @@ const UserForm = () => {
                                         <tr>
                                             <td>{data.fname + " " + data.lname}</td>
                                             <td>{data.email}</td>
-                                            <td>{data.contact}</td>
+                                            <td>{data.dept}</td>
+                                            <td>{data.des}</td>
                                             <td>{data.city}</td>
+                                            <td><button type="submit" className="btn btn-success" onClick={() => handleSelect(data.id)}>Select</button>
+                                                <button type="submit" className="btn btn-primary mx-3 my-2" onClick={() => handleEdit(data.id)}>Edit</button>
+                                                <button type="submit" className="btn btn-danger" onClick={() => handleDelete(data.id)}>Delete</button>
+                                            </td>
                                         </tr>
                                     </tbody>
                                 </>
